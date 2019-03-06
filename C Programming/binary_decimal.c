bb91eb8b2e0c5afc08ec29d3e5e96778c6cffb0d
Grammonde <45678451+klutz2121@users.noreply.github.com>
2019-03-06 16:16:20 +0700

    Create binary_decimal.c

---
+++ C Programming/binary_decimal.c
@@ -0,0 +1,20 @@
+//This is a programme of covertion between binary and decimal number
+
+#include <stdio.h>
+#include <math.h>
+main()
+{
+	int a,b,c=0,d,e,f,i;
+	
+	printf("Please enter the binary number:");
+	scanf("%d",&a);
+	i=0;
+	for(;a>0;)
+	{
+		b=a%10;
+		a=a/10;
+		c=b*pow(2,i)+c;
+		i++;
+	}
+	printf("The decimal is:%d",c);
+}
