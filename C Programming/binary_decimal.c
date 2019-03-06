1b01377bd66563cb44ad4f791fa704a072ea23ca
Grammonde <45678451+klutz2121@users.noreply.github.com>
2019-03-06 16:27:41 +0700

    Update binary_decimal.c

--- C Programming/binary_decimal.c
+++ C Programming/binary_decimal.c
@@ -2,19 +2,19 @@
 
 #include <stdio.h>
 #include <math.h>
+
 main()
 {
-	int a,b,c=0,i;
+	int binary,digit,decimal=0,i=0;
 	
 	printf("Please enter the binary number:");
-	scanf("%d",&a);
-	i=0;
+	scanf("%d",&binary);
 	for(;a>0;)
 	{
-		b=a%10;
-		a=a/10;
-		c=b*pow(2,i)+c;
+		digit=binary%10;
+		binary=binary/10;
+		decimal=digit*pow(2,i)+c;
 		i++;
 	}
-	printf("The decimal is:%d",c);
+	printf("The decimal is:%d",decimal);
 }
