9140ec492b862dda1a3ad797cf183b18ebdbb79b
lychunvira18 <lychunvira18@kit.edu.kh>
2019-03-06 15:57:44 +0700

    Update decimal-to-binary.c
    Changed temp[50] to bin[50] and removed bin

--- C Programming/decimal-to-binary.c
+++ C Programming/decimal-to-binary.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
 int main(){
-  int dec, bin, temp[50], i=0, j;
+  int dec, bin[50], i=0, j;
   printf("Enter the decimal number : ");
   scanf("%d",&dec);
 
   do{
-    temp[i]=dec%2;
+    bin[i]=dec%2;
     dec/=2;
     i++;
   }while(dec>0);
 
   printf("The binary number is : ");
   for(j=i-1;j>=0;j--){
-    printf("%d",temp[j]);
+    printf("%d",bin[j]);
   }
 
   return 0;
